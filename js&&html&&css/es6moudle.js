b183c8eafe0ef1617a1512a0ad36f97b07353d49
songzhen5656 <1244418178@qq.com>
2019-05-27 13:58:45 +0800

    修改

--- js&&html&&css/es6moudle.js
+++
@@ -1,8 +0,0 @@
-import {firstName,lastName,year,getData} from 'js/es6Test.js'
-var x = 1;
-var y = 3;
-console.log(firstName);
-console.log(lastName);
-console.log(year)
-console.log(getData(x,y));
-
--- js&&html&&css/js/es6Test.js
+++
@@ -1,9 +0,0 @@
-var firstName = 'song';
-var lastName = 'zhen';
-var year = 1995;
-
-function getData(x,y){
-	return x + y;
-}
-
-export {firstName,lastName,year,getData};
